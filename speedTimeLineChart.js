7c458aae570f7bedca7043c769d68fe6b44505c3
eh250 <eh250@pc7-060-l.cs.st-andrews.ac.uk>
2022-03-31 16:39:26 +0100

    Added axis scaling to lineChart file.

--- speedTimeLineChart.js
+++ speedTimeLineChart.js
@@ -5,55 +5,38 @@
 //let xMid = width / 2;
 //let yMid = height / 2;
 
-d3.csv(datapath)
+//consider converting lap time stamp into constituent parts
+//not agreeing with Date.parse in current form
+let timeConverter = (d) => {
+    let lapTime = d.Time.substring(10,18);
+    let periods = lapTime.split(":");
+
+    return {
+        mins: parseFloat(periods[0]),
+        secs: parseFloat(periods[1]),
+        totalSec: parseInt(periods[0])*60 + parseInt(periods[1]),
+        speed: +d.Speed
+    }
+}
+
+d3.csv(datapath, timeConverter)
     .then(function (myData) {
-        console.log(myData);
+
+        let timeExtent = d3.extent((d) => d.totalSec);
+        let xScale = d3.scaleLinear()
+                            .domain(timeExtent)
+                            .range([0, width]);
+        
+        let speedExtent = d3.extent((d) => d.Speed);
+        let yScale = d3.scaleLinear()
+                            .domain(speedExtent)
+                            .range([0, height]);
+
+        let x_axis = d3.axisBottom(xScale);
+        let y_axis = d3.axisLeft(yScale);
 
         let scatter_svg = d3.select("#chart2")        //creates an SVG element in the body
             .append("svg")
             .attr("width", width + margin)
             .attr("height", height + margin);
-        
-        myData.forEach((d) => console.log(Date.parse(d.Time.substring(10,18))));
-        
-        //consider converting lap time stamp into constituent parts
-        //not agreeing with Date.parse in current form
-        let timeConverter = (d) => {
-            let lapTime = d.Time.substring(10,18);
-            let periods = lapTime.split(":");
-
-            return {
-                mins: parseInt(periods[0]),
-                secs: parseInt(periods[1]),
-                ms: parseInt(periods[2]),
-                totalms: parseInt(periods[0])*60*100 + parseInt(periods[1])*100 + parseInt(periods[2]),
-                speed: d.Speed
-            }
-        }
-
-        //convert X and Y attributes back to integers
-        myData.forEach(function (d) {
-            d.X = +d.X;
-            d.Y = +d.Y;
-        });
-
-        let xMax = d3.max(myData, (d) => d.X);
-        let yMax = d3.max(myData, (d) => d.Y);
-        let speedMax = d3.max(myData, (d) => parseInt(d.Speed));
-				const colorScale = d3.scaleLinear()
-										.domain([0, speedMax])
-										.range(["#FFFF00","#FF0000"]);
-
-        d3.select("scatter_svg")
-            .selectAll("circle")
-            .data(myData)
-            .enter().append("circle")
-            .style("fill", "black")
-            .attr("cx", function (d) {
-                return xMid + xMid * (d.X / xMax);
-            })
-            .attr("cy", function (d) {
-                return yMid + yMid * (d.Y / yMax);
-            })
-            .attr("r", 2);
     });
