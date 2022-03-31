1b396653004e02bc37693bb0fb8a427e52708a5f
eh250 <eh250@pc7-060-l.cs.st-andrews.ac.uk>
2022-03-31 11:42:48 +0100

    Added basic hover over speed label to heatmap.

--- trackHeatmap.js
+++ trackHeatmap.js
@@ -25,9 +25,9 @@ d3.csv(datapath)
         let xMax = d3.max(myData, (d) => d.X);
         let yMax = d3.max(myData, (d) => d.Y);
         let speedMax = d3.max(myData, (d) => d.Speed);
-				const colorScale = d3.scaleLinear()
-										.domain([0, speedMax])
-										.range(["#FFFF00","#FF0000"]);
+		const colorScale = d3.scaleLinear()
+									.domain([0, speedMax])
+									.range(["#FFFF00","#FF0000"]);
 
         d3.select("svg")
             .selectAll("circle")
@@ -40,5 +40,22 @@ d3.csv(datapath)
             .attr("cy", function (d) {
                 return yMid + yMid * (d.Y / yMax);
             })
-            .attr("r", 2);
+            .attr("r", 2)
+            .on("mouseenter", function(event, d){
+                d3.select(this)
+                    .style("fill", "#000000");
+                d3.select("svg")
+                    .append("text")
+                        .attr("class", "tooltip")
+                        .attr("x", event.x)
+                        .attr("y", event.y - 20)
+                        .attr("data-html", "true")
+                        .text("Speed: " + d.Speed + "km/h");
+            })
+            .on("mouseout", function(event, d){
+                d3.select(this)
+                    .style("fill", (d) => colorScale(d.Speed));
+                d3.selectAll(".tooltip")
+                    .remove();
+            });
     });
