101cb48c59e09c4ea3f62ae51cbd4e1e97226fbd
jamesvint <jvint1997@hotmail.co.uk>
2022-04-03 21:35:25 +0100

    Integrating Charts

--- speedTimeLineChart.js
+++ speedTimeLineChart.js
@@ -1,3 +1,7 @@
+let graphWidth = 400;					//specifies the width, height and margins of our SVG element
+let graphHeight = 240;
+let graphMargin = 40;
+
 let timeConverter = (d) => {
     let lapTime = d.Time.substring(10,18);
     let periods = lapTime.split(":");
@@ -25,12 +29,12 @@ function createLineGraph(){
             let timeExtent = d3.max(myData, (d) => +d.totalSec);
             let xScale = d3.scaleLinear()
                 .domain([0, timeExtent])
-                .range([0, width]);
+                .range([0, graphWidth]);
 
             let speedMax = d3.max(myData, (d) => d[selectValue]);
             let yScale = d3.scaleLinear()
                 .domain([0, speedMax])
-                .range([height, 50]);
+                .range([graphHeight, 50]);
 
             let x_axis = d3.axisBottom(xScale);
             let y_axis = d3.axisLeft(yScale);
@@ -38,50 +42,35 @@ function createLineGraph(){
             let scatter_svg = d3.select("#chart3")        //creates an SVG element in the body
                 .append("svg")
                 .attr("id", "lineSVG")
-                .attr("width", width + margin)
-                .attr("height", height + margin);
+                .attr("width", graphWidth + graphMargin)
+                .attr("height", graphHeight + graphMargin);
 
             scatter_svg.append("g")
                 .attr("class", "x axis")
-                .attr("transform", `translate(${margin}, ${height})`)
+                .attr("transform", `translate(${graphMargin}, ${graphHeight})`)
                 .call(x_axis);
 
             scatter_svg.append("g")
                 .attr("class", "y axis")
-                .attr("transform", `translate(${margin}, 0)`)
+                .attr("transform", `translate(${graphMargin}, 0)`)
                 .call(y_axis);
 
             d3.select(".x.axis")
                 .append("text")
                 .text("Time - seconds")
                 .style("fill", "black")
-                .attr("x", (width - margin)/2)
-                .attr("y", margin-50);
+                .attr("x", (graphWidth - graphMargin)/2)
+                .attr("y", graphMargin-50);
 
             d3.select(".y.axis")
                 .append("text")
                 .text(selectValue)
                 .style("fill", "black")
-                .attr("transform", `rotate(-90,0,${margin-50}) translate(${-margin}, 0)`);
+                .attr("transform", `rotate(-90,0,${graphMargin-50}) translate(${-graphMargin}, 0)`);
 
             let lineGenerator = d3.line()
-                .x((d) => margin + xScale(+d.totalSec))
+                .x((d) => graphMargin + xScale(+d.totalSec))
                 .y((d) => yScale(d[selectValue]));
-
-
-            function updateLineTitle(title) {
-                d3.select("#lineSVG")
-                    .append("text")
-                    .attr("id", "chartTitle")
-                    .attr("x", (width / 2))
-                    .attr("y", 16)
-                    .attr("text-anchor", "middle")
-                    .style("fill", "black")
-                    .style("font-size", "16px")
-                    .style("text-decoration", "underline")
-                    .text(title + " at given time")
-            }
-            updateLineTitle(selectValue)
 
             scatter_svg.append("path")
                 .datum(myData)
